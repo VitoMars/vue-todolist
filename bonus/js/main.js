ed3eeaabf724fb32998d3eeb3023537ba108ca2e
Vito <vito95v@gmail.com>
2021-09-09 17:14:04 +0200

    Inserimento cartella bonus

---
+++ bonus/js/main.js
@@ -0,0 +1,23 @@
+const app = new Vue({
+   el: "#root",
+   data: {
+      newToDo: "",
+      toDos: [
+         "Fare la spesa",
+         "Fare il bucato",
+         "Fare i compiti",
+         "Fare schifo",
+      ],
+   },
+   methods: {
+      addToDo() {
+         if (this.newToDo != "") {
+            this.toDos.push(this.newToDo);
+            this.newToDo = "";
+         }
+      },
+      removeToDo(index) {
+         this.toDos.splice(index, 1);
+      },
+   },
+});
