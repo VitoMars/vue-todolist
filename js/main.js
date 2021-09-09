9649897a56653e2ed4afa5433df5986cebb2c46a
Vito <vito95v@gmail.com>
2021-09-09 16:30:50 +0200

    Aggiunto metodo "addToDo"

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 const app = new Vue({
    el: "#root",
    data: {
+      newToDo: "",
       toDos: [
          "Fare la spesa",
          "Fare il bucato",
          "Fare i compiti",
          "Fare schifo",
       ],
+   },
+   methods: {
+      addToDo() {
+         if (this.newToDo != "") {
+            this.toDos.push(this.newToDo);
+            this.newToDo = "";
+         }
+      },
    },
 });
